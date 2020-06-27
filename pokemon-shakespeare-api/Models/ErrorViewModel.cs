50a28953aaede2447d5182b773f76ecc762fc2b9
Sean Jackson <sean.jackson24@gmail.com>
2020-06-27 15:19:15 +0100

    rescaffold API project

---
+++ pokemon-shakespeare-api/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace pokemon_shakespeare_api.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
